24f28ca22136a9b6964005b5bbb0633545c48f53
san.mateo14dc <san.mateo14dc@gmail.com>
2026-04-07 19:04:35 +0000

    Corrección del montura de Render en el server.js

--- backend/server.js
+++ backend/server.js
@@ -159,9 +159,10 @@ const initialize = async () => {
     });
 
     // Iniciar servidor
-    const PORT = process.env.BACKEND_PORT || 5000;
-    app.listen(PORT, () => {
-      console.log(`🚀 Backend en http://localhost:${PORT}`);
+    const PORT = process.env.PORT || process.env.BACKEND_PORT || 5000;
+
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`🚀 Backend en http://0.0.0.0:${PORT}`);
     });
 
   } catch (error) {
