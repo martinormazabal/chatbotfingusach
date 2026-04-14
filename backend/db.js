73b996e2e5b63c1ce1e7019beb3c80db4facdf77
san.mateo14dc <san.mateo14dc@gmail.com>
2026-04-14 14:36:07 +0000

    solucionando el ingreso de datos a Supabase.

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,8 @@
-// backend/db.js
-const path = require("path");
 const { Pool } = require("pg");
-require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
